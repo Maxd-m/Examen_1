b93f4c20ac8b605c41a2c732ed8507393ff44251
MaxDM <maxdiazm.2004@gmail.com>
2026-02-19 19:48:40 -0600

    validations added to QR endpoints

--- domains/QrCode/resource/QRCodeResource.php
+++ domains/QrCode/resource/QRCodeResource.php
@@ -19,10 +19,33 @@ class QRCodeResource
         header("Content-Type: application/json");
 
         $data = json_decode(file_get_contents("php://input"), true);
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            http_response_code(415);
+            echo json_encode([
+                "message" => "Body (JSON) inválido"
+            ]);
+            return;
+        }
 
         if (empty($data['content'])) {
             http_response_code(400);
             echo json_encode(["message" => "Campo 'content' requerido"]);
+            return;
+        }
+
+        //validar que size este entre 100 y 1000
+        if (isset($data['size']) && ($data['size'] < 100 || $data['size'] > 1000)) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campo 'size' debe estar entre 100 y 1000"]);
+            return;
+        }
+
+        //validar que error level sea uno de L, M, Q, H
+        if (isset($data['errorLevel']) && !in_array($data['errorLevel'], ['L', 'M', 'Q', 'H'])) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campo 'errorLevel' debe ser uno de L, M, Q, H"]);
             return;
         }
 
@@ -45,6 +68,14 @@ class QRCodeResource
 
         $data = json_decode(file_get_contents("php://input"), true);
 
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            http_response_code(415);
+            echo json_encode([
+                "message" => "Body (JSON) inválido"
+            ]);
+            return;
+        }
+
         if (empty($data['URL'])) {
             http_response_code(400);
             echo json_encode(["message" => "Campo 'URL' requerido"]);
@@ -55,6 +86,20 @@ class QRCodeResource
         if (!filter_var($data['URL'], FILTER_VALIDATE_URL)) {
             http_response_code(400);
             echo json_encode(["message" => "URL no válida"]);
+            return;
+        }
+
+        //validar que size este entre 100 y 1000
+        if (isset($data['size']) && ($data['size'] < 100 || $data['size'] > 1000)) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campo 'size' debe estar entre 100 y 1000"]);
+            return;
+        }
+
+        //validar que error level sea uno de L, M, Q, H
+        if (isset($data['errorLevel']) && !in_array($data['errorLevel'], ['L', 'M', 'Q', 'H'])) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campo 'errorLevel' debe ser uno de L, M, Q, H"]);
             return;
         }
 
@@ -77,9 +122,46 @@ class QRCodeResource
 
         $data = json_decode(file_get_contents("php://input"), true);
 
+                if (json_last_error() !== JSON_ERROR_NONE) {
+            http_response_code(415);
+            echo json_encode([
+                "message" => "Body (JSON) inválido"
+            ]);
+            return;
+        }
+
         if (empty($data['SSID']) || empty($data['password'])) {
             http_response_code(400);
             echo json_encode(["message" => "Campos 'SSID' y 'password' requeridos"]);
+            return;
+        }
+
+        if (strlen($data['SSID']) > 35 || strlen($data['password']) > 64) {
+            http_response_code(413);
+            echo json_encode([
+                "message" => "SSID o password demasiado largos"
+            ]);
+            return;
+        }
+
+        //validar que encryption sea uno de WEP, WPA, nopass
+        if (isset($data['encryption']) && !in_array($data['encryption'], ['WEP', 'WPA'])) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campo 'encryption' debe ser uno de WEP, WPA, nopass"]);
+            return;
+        }
+
+        //validar que size este entre 100 y 1000
+        if (isset($data['size']) && ($data['size'] < 100 || $data['size'] > 1000)) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campo 'size' debe estar entre 100 y 1000"]);
+            return;
+        }
+
+        //validar que error level sea uno de L, M, Q, H
+        if (isset($data['errorLevel']) && !in_array($data['errorLevel'], ['L', 'M', 'Q', 'H'])) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campo 'errorLevel' debe ser uno de L, M, Q, H"]);
             return;
         }
 
@@ -107,9 +189,38 @@ class QRCodeResource
 
         $data = json_decode(file_get_contents("php://input"), true);
 
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            http_response_code(415);
+            echo json_encode([
+                "message" => "Body (JSON) inválido"
+            ]);
+            return;
+        }
+
         if (empty($data['latitude']) || empty($data['longitude'])) {
             http_response_code(400);
             echo json_encode(["message" => "Campos 'latitude' y 'longitude' requeridos"]);
+            return;
+        }
+
+        //validar formato de coordenadas (deben ser números válidos)
+        if (!is_numeric($data['latitude']) || !is_numeric($data['longitude'])) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campos 'latitude' y 'longitude' deben ser números válidos"]);
+            return;
+        }
+
+        //validar que size este entre 100 y 1000
+        if (isset($data['size']) && ($data['size'] < 100 || $data['size'] > 1000)) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campo 'size' debe estar entre 100 y 1000"]);
+            return;
+        }
+
+        //validar que error level sea uno de L, M, Q, H
+        if (isset($data['errorLevel']) && !in_array($data['errorLevel'], ['L', 'M', 'Q', 'H'])) {
+            http_response_code(400);
+            echo json_encode(["message" => "Campo 'errorLevel' debe ser uno de L, M, Q, H"]);
             return;
         }
 
